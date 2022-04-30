b08642de1120a8864f66ef0787899a5fca1148ac
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-30 11:18:26 +0100

    updates

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`server listening on port ${port}`));
+app.listen(port, console.log(`server listening on port ${port}`)); 
